e56a0dee3ed5c1a928401bba4afa21c1c3b8e647
nombre <daniel.b100726@gmail.com>
2021-06-07 13:47:20 -0400

    Updates

---
+++ precache-manifest.f4599da6354319fafb0bb4247d8e6f61.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a599f57e4109cc2a1dd",
+    "url": "/Curso-React-JS-platzi-badges/static/css/main.bf3301bf.chunk.css"
+  },
+  {
+    "revision": "4a599f57e4109cc2a1dd",
+    "url": "/Curso-React-JS-platzi-badges/static/js/main.4a599f57.chunk.js"
+  },
+  {
+    "revision": "cd4643d76d796bd5d147",
+    "url": "/Curso-React-JS-platzi-badges/static/js/runtime~main.cd4643d7.js"
+  },
+  {
+    "revision": "0348bd214df0d750cd2a",
+    "url": "/Curso-React-JS-platzi-badges/static/css/2.441602b3.chunk.css"
+  },
+  {
+    "revision": "0348bd214df0d750cd2a",
+    "url": "/Curso-React-JS-platzi-badges/static/js/2.0348bd21.chunk.js"
+  },
+  {
+    "revision": "d8917de2e02fbfa50ca36277734f877a",
+    "url": "/Curso-React-JS-platzi-badges/static/media/ingress5.d8917de2.png"
+  },
+  {
+    "revision": "2de2bfa1380cb84d1f67e51bf1b79b5c",
+    "url": "/Curso-React-JS-platzi-badges/static/media/twitter.2de2bfa1.svg"
+  },
+  {
+    "revision": "edac52f424233faabce3d4fab87728e3",
+    "url": "/Curso-React-JS-platzi-badges/static/media/space.edac52f4.png"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/Curso-React-JS-platzi-badges/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Curso-React-JS-platzi-badges/static/media/react-logo3.06e73328.svg"
+  },
+  {
+    "revision": "772e3ffaae6b4771307f3ae323762366",
+    "url": "/Curso-React-JS-platzi-badges/static/media/error-500.772e3ffa.png"
+  },
+  {
+    "revision": "3e2b05c7b161e983b777dfdda74e70eb",
+    "url": "/Curso-React-JS-platzi-badges/static/media/Error404.3e2b05c7.jpg"
+  },
+  {
+    "revision": "3d899b30f85999c18457a87ecd0af17e",
+    "url": "/Curso-React-JS-platzi-badges/static/media/Dr.Stone.3d899b30.jpg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/Curso-React-JS-platzi-badges/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "b6a1e5f339f7b33a669097eebbd4a299",
+    "url": "/Curso-React-JS-platzi-badges/index.html"
+  }
+];
