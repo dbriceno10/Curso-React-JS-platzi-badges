0c09c975292f52e4e001c7acab2f850975ab1774
nombre <daniel.b100726@gmail.com>
2021-06-13 19:29:53 -0400

    uso de api en la lista de personajes

--- src/pages/badges.js
+++ src/pages/badges.js
@@ -9,13 +9,19 @@ import api from "../api"
 import PageLoading from "../componets/pageLoading"
 import PageError from "../componets/pageError"
 import MiniLoader from "../componets/miniLoader"
+import "../componets/styles/BadgesList.css"
+import "bootstrap/dist/css/bootstrap.css"
+import twitterLogo from "../images/twitter.svg"
+// import getData from "../utils/getData"
+// import getHash from "../utils/getHash"
 //NOTA: Borré algunas notas sobre el ciclo de vida de los componentes, si quieres revisarlos regresa a ese commit o ve la notas
+
 class Badges extends React.Component {
-    state = {//ahora vamos a traer los datos desde la api, se inicializan como undefinded
-        loading: true,
-        error: null,
-        data: undefined
-    }
+    // state = {//ahora vamos a traer los datos desde la api, se inicializan como undefinded
+    //     loading: true,
+    //     error: null,
+    //     data: undefined
+    // }
 
     // constructor(props) {//El constructor recibe props, el cual necesitamos para inicializar la SuperClase
     //     super(props)
@@ -24,28 +30,48 @@ class Badges extends React.Component {
     //         data: [],//estado inicial vacío
     //     }
     // }
-
-    //console.log("3.componentDidMount()")
-        //Este componente es el lugar adecuado para realizar una petición de datos, o actualización
+    state = {
+        loading: true,
+        error: null, 
+        data: {
+            results: [],
+        },
+        nextPage: 1
+    }
     componentDidMount() {
-        this.intervalID = setInterval(this.fetchData, 10000)//Hay que cancelarlo cuando la página se vaya
+        //this.intervalID = setInterval(this.fetchData, 10000)//Hay que cancelarlo cuando la página se vaya
+        this.intervalID = this.fetchData()
     }
 
     componentWillUnmount() {
         clearInterval(this.intervalID)
-        //con esto evitamos que el intervalo siga trabajando aún cuando dejamos la página
     }
 
-    //Este fetchData al que hacemos referencia (con this) es un método de la clase que estamos declarando, no el fetch nativo de javascript
     fetchData = async () => {
-        this.setState({ loading: true, error: null })//Declaramos el estado de loading en true y error en null, si no es la primera vez que se llama, se vuelve a pasar loading como true, y si existía un error este se cancela
-
+        this.setState({ loading: true, error: null })//
         try {
-            const data = await api.badges.list()
-            this.setState({ loading: false, data: data })//si obtenemos los datos los guardamos
-        } catch(error) {
-        this.setState({ loading: false, error: error })
+            const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`)
+            const data = await response.json()
+
+            this.setState({
+                loading: false,
+                data: {
+                    info: data.info,
+                    results: [].concat(this.state.data.results, data.results)
+                },
+                nextPage: this.state.nextPage + 1
+            })
+        } catch (error) {
+            console.error(error)
+            this.setState({ loading: false, error: error })
         }
+
+        // try {
+        //     const data = await api.badges.list()
+        //     this.setState({ loading: false, data: data })//si obtenemos los datos los guardamos
+        // } catch(error) {
+        // this.setState({ loading: false, error: error })
+        // }
     }
     //junto a esa actualiación se llama a:
     // componentDidUpdate(propsPrevios, estadoPrevio){
@@ -88,33 +114,35 @@ class Badges extends React.Component {
             return <PageError error={this.state.error} />
         }
         return (
-            <div className="fondo">
-                {/* <div className="Badges">
-                    <div className="Badges__hero">
-                        <div className="Badges__container">
-                            <div className="badge-logos-container">
-                                <Link to="/comingson"><img className="Badges_conf-logo Badges_conf-logo-width" src={ConfLogo} alt="Conf Logo" /></Link>
-                                <a href="https://es.reactjs.org/" target="_blank"><img className="Badges_conf-logo react-logo" src={ReactLogo} alt="Logo de React js" /></a>
-                            </div>
-                            <div className="Badges__buttons">
-                                
-                                <Link to="/badges/new" className="btn btn-primary">New Badge</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-
+            <React.Fragment>
                 <div className="Badges__hero">
                     <div className="Badges__container">
-                        {/* <div className="Badges__buttons">
-                            <Link to="/badges/new" className="btn btn-primary">New Badge</Link>
-                        </div> */}
-
-                        
-                        
                         <div className="Badges__list">
                             <div className="Badges__container">
-                                <BadgesList badges={this.state.data}/>
+                                {/* <BadgesList badges={this.state.data}/> */}
+                                <ul className="list-unstyled BadgesList">
+                                    {this.state.data.results.map((character) => {
+                                        return (
+                                            <li key = {character.id} className="BadgesListItem">
+                                                <img src={character.image} alt={character.name} className="BadgesListItem__avatar" />
+                                                <div>
+                                                    <b>
+                                                        {character.name}
+                                                    </b>
+                                                    <div className="Twitter__name">
+                                                    <span className="Twitter__logo"><img className="Twitter__logo-img" src={twitterLogo} alt="logo de twitter"/></span>
+                                                    @{character.name}
+                                                    </div>
+                                                    {character.species}    
+                                                </div>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                                {this.state.loading && <div className="miniloader"><MiniLoader/></div>}
+                                {!this.state.loading && this.state.data.info.next && (<div className="Badges__buttons"><button className="btn btn-primary" onClick= {
+                                    () => {this.fetchData()}
+                                }>Loading More</button></div>)}
                                 <div className="Badges">
                                     <div className="Badges__hero">
                                         <div className="Badges__container">
@@ -143,7 +171,7 @@ class Badges extends React.Component {
                     </div>
                 </div>
                 
-            </div>
+            </React.Fragment>
         )
     }
 }
---
+++ src/utils/getData.js
@@ -0,0 +1,63 @@
+// const getData = async () => {
+//     this.setState({ loading: true, error: null })
+//     try {
+//         const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`)
+//         const data = await response.json()
+
+//         this.setState({
+//             loading: false,
+//             data: {
+//                 info: data.info,
+//                 results: [].concat(this.state.data.results, data.results)
+//             },
+//             nextPage: this.state.nextPage + 1
+//         })
+//     } catch (error) {
+//         console.error(error)
+//         this.setState({ loading: false, error: error})
+//     }
+    
+// }
+
+// export default getData
+
+
+
+// const getData = async () => {
+//     this.setState({ loading: true, error: null });//estamos cargando datos
+
+//     try {
+//         const response = await fetch(
+//         `https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`//luego le vamos a estar pasando el número de la siguiente página
+//         );
+//         const data = await response.json();
+
+//         this.setState({//vamos a guardar los datos acá
+//             loading: false,
+//             data: {
+//             info: data.info,
+//             results: [].concat(this.state.data.results, data.results)//aquí estamos concatenando (concat) los resultados nuevos que llegaron con los que ya estaban antes, para sí mostrarlos todos en la misma página
+//             },
+//             nextPage: this.state.nextPage + 1//aumentamos el valor de la página en 1
+//         });
+//     } catch (error) {
+//         this.setState({ loading: false, error: error })
+//     }
+// }
+
+// export default getData
+
+const API = "https://rickandmortyapi.com/api/character/"
+
+const getData = async (id) => {
+    const apiURL = id ? `${API}${id}` : API
+    try {
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        return data
+    } catch(error) {
+        console.error("Fetch Error", error)
+    }
+}
+
+export default getData
