b8584ff620beb9e02039029b0d9a443810ef6555
nombre <daniel.b100726@gmail.com>
2021-06-14 16:47:31 -0400

    ajustando las rutas

--- src/componets/pageError.js
+++ src/componets/pageError.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom" 
 import "./styles/PageError.css"
+import "../pages/styles/Badges.css"
 import error500 from "../images/error-500.png"
 
 function PageError(props) {
   // return <div className="PageError">❌{props.error.message}😱</div>;
   return (
     <div className="PageError">
-    <Link className="text-reset text-decoration-none" to={`/Curso-React-JS-platzi-badges/`}>
-      <img className="error-img" src={error500} alt="server-internal error" />
+      <Link to={`/Curso-React-JS-platzi-badges/`}>
+        <img className="error-img" src={error500} alt="server-internal error" />
+      </Link>
       <h1 className="h1-title">{props.error.message}, please reload the page.</h1>
-    </Link>
-      
+      <div className="Badges__buttons">
+        <Link className="btn btn-primary" to={`/Curso-React-JS-platzi-badges/`}>Home</Link>
+      </div>
     </div>
   )
 }
